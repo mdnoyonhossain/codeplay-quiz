83dc4dae536662259b3fed714f3eb93551546319
MD Shahinur Rahman Nayon <mdshahinurrahmannayon@gmail.com>
2022-10-10 21:37:47 +0600

    design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Topic from './components/Topic/Topic';
 
 function App() {
+
   return (
     <div>
-     this is react application quize
+     <Header></Header>
+     <Home></Home>
+     <Topic></Topic>
+     <Footer></Footer>
     </div>
   );
 }
